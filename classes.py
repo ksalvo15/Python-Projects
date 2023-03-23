5d7e2901b05513b34faf50149de4db051d568b26
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-03-23 11:52:10 -0700

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,16 +1,16 @@
 #people at a university
-class university_user
+class university_user:
     name = ''
     email = ''
     password = ''
     username = ''
 
 #students at a university
-class student:
+class student(university_user):
     classes = ''
     year = ''
 
 #teachers at a university
-class teacher:
+class teacher(university_user):
     department = ''
     building = ''

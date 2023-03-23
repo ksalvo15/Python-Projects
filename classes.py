01d98a99397ec92b1f4fb9f75da23fcf9da20977
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-03-23 13:05:57 -0700

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,16 +1,47 @@
 #people at a university
 class university_user:
-    name = ''
-    email = ''
-    password = ''
-    username = ''
+    University_name = 'Portland State'
+    email_format = 'fnamelname@portlandstate.edu'
+    fname = ''
+    lname = ''
+    year =''
+    Teacher =''
+    building =''
+    classes = ''
 
+    def information(self):
+        msg = "\nUniversity: {}\nEmail Format: {}\nFirst Name: {}\nLast Name: {}\nyear: {}\nTeacher: {}\nbuilding: {}\nclasses: {}".format(self.University_name,self.email_format,self.fname,self.lname,self.year,self.Teacher,self.building,self.classes)
+        return msg
+      
 #students at a university
 class student(university_user):
-    classes = ''
-    year = ''
+    fname = 'jane'
+    lname = 'smith'
+    classes = 'Math'
+    year = 'Sopomore'
+    Teacher = False
+
+    def dorms(self):
+        msg = "they are a student here"
+        return msg
 
 #teachers at a university
 class teacher(university_user):
-    department = ''
-    building = ''
+    building = 'Building A'
+    fname = 'Bill'
+    lname = 'smith'
+    teacher = True
+
+
+
+
+
+
+if __name__=="__main__":
+
+    student1 = student()
+    print(student1.information())
+    print(student1.dorms())
+        
+        
+    

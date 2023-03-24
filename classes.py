77e00a4cb5024b4f4d28313f304743964a1c0d02
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-03-24 11:15:39 -0700

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,17 +1,28 @@
 #people at a university
 class university_user:
     University_name = 'Portland State'
-    email_format = 'fnamelname@portlandstate.edu'
     fname = ''
     lname = ''
+    email_format = '{}{}@portlandstate.edu'.format(fname,lname) #need to fix this for later
     year =''
     Teacher =''
     building =''
     classes = ''
+    password = 'password1234'
 
     def information(self):
-        msg = "\nUniversity: {}\nEmail Format: {}\nFirst Name: {}\nLast Name: {}\nyear: {}\nTeacher: {}\nbuilding: {}\nclasses: {}".format(self.University_name,self.email_format,self.fname,self.lname,self.year,self.Teacher,self.building,self.classes)
+        msg = "\nUniversity: {}\nFirst Name: {}\nLast Name: {}\nEmail Format: {}\nyear: {}\nTeacher: {}\nbuilding: {}\nclasses: {}".format\
+              (self.University_name,self.fname,self.lname,self.email_format,self.year,self.Teacher,self.building,self.classes)
         return msg
+
+    def login(self):
+        user_name = input("enter your username")
+        user_email = input("enter your email")
+        user_password = input("enter your password")
+        if (user_email ==self.email_format and user_password ==self.password):
+            print("welcome back {}".format(user_name))
+        else:
+            print("the user email or password is incorrect")
       
 #students at a university
 class student(university_user):
@@ -30,9 +41,18 @@ class teacher(university_user):
     building = 'Building A'
     fname = 'Bill'
     lname = 'smith'
+    email_format = 'billsmith@portlandstate.edu'
     teacher = True
+    pin = '1234'
 
-
+    def login(self):
+        user_name = input("enter your username")
+        user_email = input("enter your email")
+        user_pin = input("enter your teacher pin")
+        if (user_email ==self.email_format and user_pin ==self.pin):
+            print("welcome back {}".format(user_name))
+        else:
+            print("the user email or pin is incorrect")
 
 
 
@@ -42,6 +62,12 @@ if __name__=="__main__":
     student1 = student()
     print(student1.information())
     print(student1.dorms())
+
+    teachers= teacher()
+    teachers.login()
+    
+    students = university_user()
+    students.login()
         
         
     

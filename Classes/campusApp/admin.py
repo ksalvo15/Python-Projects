b6bbf976c9f52acffa9fba19f8bdbb55d89f06f2
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-04-21 11:09:00 -0700

    DjangoUniversity Project
    added a Campus and Classes App

---
+++ Classes/campusApp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import UniversityCampus
+
+# Register your models here.
+"""#this registers the admin site onto the website"""
+admin.site.register(UniversityCampus)
+

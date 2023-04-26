f0bee3be960623bb61c10396b8485b5be6254f42
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-04-26 13:11:08 -0700

    edited the classes project

--- DjangoUniversity/Classes/campusApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UniversityCampus
-
-# Register your models here.
-"""#this registers the admin site onto the website"""
-admin.site.register(UniversityCampus)
-
